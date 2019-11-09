99f8380387d4c8a0efea249267b135e2b77d5d7f
Kris <krystian.rytel@gmail.com>
2019-11-09 18:30:53 +0100

    Start

---
+++ deck.py
@@ -0,0 +1,14 @@
+class Deck:
+
+    def __init__(self):
+        # TODO: Dopisać karty
+        self.cards = [2, 2, 2]
+        self.shuffleCards()
+
+    def shuffleCards(self):
+        # TODO: pomieszać karty
+        x = 5
+
+    def getTopCard(self):
+        # TODO: zwrócenie górnej karty
+        x = 5
---
+++ game.py
@@ -0,0 +1,14 @@
+from deck import Deck
+
+class Game:
+
+    def __init__(self):
+        self.dealer_cards = []
+        self.player_cards = []
+        self.deck = Deck()
+
+    def play(self):
+        # TODO: Rozgrywka
+        x = 5
+        # TODO: dealer losuje dwie karty, pierwsza jest odkryta
+        
