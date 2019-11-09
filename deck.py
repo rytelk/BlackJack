1cef3dec7119fabe0ad4c83bfa1f7de9b461f909
adam-mata <40769841+adam-mata@users.noreply.github.com>
2019-11-09 18:59:43 +0100

    Update deck.py
    Deck initialize + card pop

--- deck.py
+++ deck.py
@@ -1,14 +1,26 @@
+import random;
+
+# ----------------------------------------------------------------------------------------------------
+class Card:
+    def __init__(self, _figure, _color):
+        self.figure = _figure;
+        self.color = _color;
+        return;
+
+# ----------------------------------------------------------------------------------------------------
 class Deck:
-
     def __init__(self):
-        # TODO: Dopisać karty
-        self.cards = [2, 2, 2]
-        self.shuffleCards()
-
-    def shuffleCards(self):
-        # TODO: pomieszać karty
-        x = 5
+        self.cards = [];
+        for color in ["TREFL", "KARO", "KIER", "PIK"]:
+            for figure in ([str(k) for k in range(2, 11)] + ["J", "Q", "K", "A"]):
+                cards.append( Card(figure, color) );
+        random.shuffle(self.cards);
+        return;
 
     def getTopCard(self):
-        # TODO: zwrócenie górnej karty
-        x = 5
+        if not self.cards:
+            print("Deck is empty, error !");
+            return;
+        self.cards.pop();
+        return;
+# ----------------------------------------------------------------------------------------------------
